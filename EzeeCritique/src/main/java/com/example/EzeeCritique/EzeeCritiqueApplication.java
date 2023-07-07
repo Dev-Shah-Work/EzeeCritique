bcba37c9f7ee781cf93394efa953c5cf67704955
Dev <dev@mail.com>
2023-07-07 14:38:03 +0530

    first commit

---
+++ EzeeCritique/src/main/java/com/example/EzeeCritique/EzeeCritiqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.EzeeCritique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzeeCritiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzeeCritiqueApplication.class, args);
+	}
+
+}
---
+++ EzeeCritique/src/test/java/com/example/EzeeCritique/EzeeCritiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EzeeCritique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzeeCritiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
